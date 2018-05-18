92afcf22f331a0a94e3f0e0f2686ea9450ee165c
jaimeabbariao <jaimeabbariao@gmail.com>
2018-05-18 12:34:06 -0400

    binary search

--- MyModule.scala
+++ MyModule.scala
@@ -47,3 +47,24 @@ object Fibonacci {
         println(fibonacci(7))
     }
 }
+
+object Search {
+    // key is our target value
+    // gt is a function that checks if one value is greater than the other
+    // this is a polymorphic function (we can pass in any type)
+    def binarySearch[A](as: Array[A], key: A, gt: (A, A) => Boolean): Int = {
+        @annotation.tailrec
+        def go(low: Int, mid: Int, high: Int): Int = {
+            if (low > high) -mid - 1
+            else {
+                val mid2 = (low + high) / 2
+                val a = as(mid2)
+                val greater = gt(a, key)
+                if (!greater && !gt(key, a)) mid2
+                else if (greater) go(low, mid2, mid2-1)
+                else go(mid2+1, mid2, high)
+            }
+        }
+        go(0, 0, as.length-1)
+    }
+}
